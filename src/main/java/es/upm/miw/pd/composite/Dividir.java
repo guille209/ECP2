1fd7312026d6ab56375f11c3ab83ca970d780165
guille209 <gguillemiranda@gmail.com>
2014-10-08 13:07:17 +0200

    Añadido dividir

---
+++ src/main/java/es/upm/miw/pd/composite/Dividir.java
@@ -0,0 +1,22 @@
+package es.upm.miw.pd.composite;
+
+import java.util.ArrayList;
+
+public class Dividir extends Expresion {
+
+	ArrayList<Expresion> expresiones;
+	
+	public Dividir(Expresion expresion,Numero numero){
+		
+	}
+
+	@Override
+	public double operar() {
+		double resultado = 0;
+		for (Expresion expresion : expresiones) {
+			resultado /= expresion.operar();
+		}
+		return resultado;
+	}
+
+}
